5885cee34e9bd07a6f1d4455e99a4edbb644cf82
Andrew Green <green.andrew.b@gmail.com>
2020-02-03 15:27:29 -0600

    big spenders baby

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
-import businesses from "./data.js"
+import businesses from "./data.js";
 
-businesses
+
+
+// big spenders exercise
+
+const bigSpendersToDOM = document.getElementById("big_spenders_container");
+
+const bigSpenders = businesses.filter(business => business.orders.find(num => num >= 9000));
+
+bigSpenders.forEach(
+    business =>
+      (bigSpendersToDOM.innerHTML += `<h2>${business.companyName}</h2>
+      <p>${business.addressFullStreet}
+      <br>${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}</p>
+      <hr>`)
+  );
+
+
+// forEach exercise
+
+const businessName = document.getElementById("business_list_container");
+
+businesses.forEach(
+  business =>
+    (businessName.innerHTML += `<h2>${business.companyName}</h2>
+    <p>${business.addressFullStreet}
+    <br>${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}</p>
+    <hr>`)
+);
